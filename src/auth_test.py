e83c8637d1b879ca922c7c688e33990ead467398
Aidan Marley <z5257171@student.unsw.edu.au>
2020-10-03 16:34:47 +1000

    Data tests and logout fixed

--- src/auth_test.py
+++ src/auth_test.py
@@ -2,6 +2,8 @@ import auth
 from other import clear
 from error import InputError
 import pytest
+from data import *
+
 
 ''' Tests:
         register:
@@ -15,12 +17,11 @@ import pytest
             2. password incorrect
         logout:
             1. correct return
-        all:
-            1. logout fails
-            2. login fails
-            3. register, login succeeds
-            4. register, logout fails
-            5. register, login, logout succeeds
+        data:
+            1. elements correct
+            2. user id in ascending order
+            3. handle < 20 characters
+            4. bighandle < 20 character
 '''
 
 def test_register_email():
@@ -58,7 +59,7 @@ def test_register_password():
     # valid password
     clear()
     auth.auth_register('validemail@gmail.com', 'valid123', 'valid', 'valid')
-    auth_login('validemail@gmail.com', 'valid123')
+    auth.auth_login('validemail@gmail.com', 'valid123')
     
 def test_register_namefirst():
     # no first name
@@ -101,15 +102,75 @@ def test_logout_success():
     # logout user that's logged in
     clear()
     auth.auth_register('validemail@gmail.com', 'valid123', 'valid', 'valid')
-    auth.auth_logout('validemail@gmail.com') == True
+    token = auth.auth_login('validemail@gmail.com', 'valid123')
+    temp = auth.auth_logout(token['token'])
+    
+    assert temp['is_success'] == True
     
     #logout bad token
     clear()
     auth.auth_register('validemail@gmail.com', 'valid123', 'valid', 'valid')
     auth.auth_logout('nonexistingtoken') == False
+
+def test_data_changes():
+    # elements stored correctly
+    clear()
+    auth.auth_register('philsmart@gmail.com', 'bigboys111', 'Phil', 'Smart')
+    
+    assert users[0]['u_id'] == 1
+    assert users[0]['email'] == 'philsmart@gmail.com'
+    assert users[0]['password'] == 'bigboys111'
+    assert users[0]['name_first'] == 'Phil'
+    assert users[0]['name_last'] == 'Smart'
+    assert users[0]['channels'] == []
+    assert users[0]['token'] == '1'
+    assert users[0]['handle'] == 'philsmart'
+    
+    auth.auth_register('darryngarryn@gmail.com', 'niceice123', 'Darryn', 'Garryn')
+    
+    assert users[1]['u_id'] == 2
+    assert users[1]['email'] == 'darryngarryn@gmail.com'
+    assert users[1]['password'] == 'niceice123'
+    assert users[1]['name_first'] == 'Darryn'
+    assert users[1]['name_last'] == 'Garryn'
+    assert users[1]['channels'] == []
+    assert users[1]['token'] == '2'
+    assert users[1]['handle'] == 'darryngarryn'
+    
+    # u_id created in ascending order
+    auth.auth_register('userid3@gmail.com', 'valid123', 'valid', 'valid')
+    auth.auth_register('userid4@gmail.com', 'valid123', 'valid', 'valid')
+    auth.auth_register('userid5@gmail.com', 'valid123', 'valid', 'valid')
+    auth.auth_register('userid6@gmail.com', 'valid123', 'valid', 'valid')
+    
+    assert users[2]['u_id'] == 3
+    assert users[3]['u_id'] == 4
+    assert users[4]['u_id'] == 5
+    assert users[5]['u_id'] == 6
+    
+    # handle cut off above 20 characters
+    
+    auth.auth_register('bighandle@gmail.com', 'valid123', 'Yothisisgonna', 'Bemassive')
+    
+    assert users[6]['handle'] == 'yothisisgonnabemassi'
+     
+    # duplicate handles
+    auth.auth_register('twohandles@gmail.com', 'valid123', 'Name', 'Name')
+    auth.auth_register('twohandles1@gmail.com', 'valid123', 'Name', 'Name')
+    
+    assert users[7]['handle'] == 'namename'
+    assert users[8]['handle'] == '9namename'
+    
+    # duplicate with big handle
+    
+    auth.auth_register('bighandle2@gmail.com', 'valid123', 'Yothisisgonna', 'Bemassive')
+    
+    assert users[9]['handle'] == '10yothisisgonnabemas'
     
     
     
+    
+     
     
     
     
