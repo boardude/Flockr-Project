fdcdf473a7900f1efb423dbea02c60b4fb883b37
Daoting <daoting.chen@student.unsw.edu.au>
2020-10-19 20:35:11 +0800

    add some cases and fix typo

--- src/user_test.py
+++ src/user_test.py
@@ -34,16 +34,16 @@ def test_user_profile():
         test for accuracy of details of returned details
     '''
     clear()
-    u1_id = auth.auth_register('test1@test.com', 'password', 'user1_name', 'user1_name')['u_id']
+    u1_id = auth.auth_register('test1@test.com', 'password', 'first_name', 'last_name')['u_id']
     token = auth.auth_login('test1@test.com', 'password')['token']
 
     assert user_profile(token, u1_id)['user']['u_id'] == u1_id
     assert user_profile(token, u1_id)['user']['email'] == 'test1@test.com'
-    assert user_profile(token, u1_id)['user']['name_first'] == 'user1_name'
-    assert user_profile(token, u1_id)['user']['name_last'] == 'user1_name'
-    assert user_profile(token, u1_id)['user']['handle_str'] == 'user1_nameuser1_name'
+    assert user_profile(token, u1_id)['user']['name_first'] == 'first_name'
+    assert user_profile(token, u1_id)['user']['name_last'] == 'last_name'
+    assert user_profile(token, u1_id)['user']['handle_str'] == 'first_namelast_name'
 
-def test_invalid_uid():
+def test_profile_invalid_uid():
     '''
         invalid uid to check
     '''
@@ -54,7 +54,18 @@ def test_invalid_uid():
     auth.auth_login('test2@test.com', 'password')
     with pytest.raises(InputError):
         user_profile(token1, u2_id)
+    with pytest.raises(InputError):
+        user_profile(token1, u2_id + 1)
 
+def test_profile_invalid_token():
+    '''
+    test the token does not refer to a valid user
+    '''
+    clear()
+    auth.auth_register('test@test.com', 'password', 'user1_name', 'user1_name')
+    u_id = auth.auth_login('test@test.com', 'password')['u_id']
+    with pytest.raises(AccessError):
+        user_profile('invalid_token', u_id)
 
 ### TEST OF SETNAME ###
 
@@ -77,8 +88,7 @@ def test_valid_setname():
     u2_id = auth.auth_register('test2@test.com', 'password',
                                'user2_first_name', 'user2_last_name')['u_id']
     token2 = auth.auth_login('test2@test.com', 'password')['token']
-    long_name = '0123456789'
-    long_name = long_name * 5
+    long_name = '0123456789' * 5
     user_profile_setname(token2, long_name, long_name)
     assert user_profile(token2, u2_id)['user']['name_first'] == long_name
     assert user_profile(token2, u2_id)['user']['name_last'] == long_name
@@ -90,42 +100,48 @@ def test_valid_setname():
     assert user_profile(token3, u3_id)['user']['name_first'] == '1'
     assert user_profile(token3, u3_id)['user']['name_last'] == '1'
 
-def test_invalid_firstname():
+def test_setname_invalid_firstname():
     '''
         invalid test of 0 characters and 26 characters of firstname
     '''
     clear()
     auth.auth_register('test1@test.com', 'password', 'user1_first_name', 'user1_last_name')
     token = auth.auth_login('test1@test.com', 'password')['token']
-    long_name = '012345678910'
-    long_name = long_name * 5
+    long_name = '0123456789' * 5 + '1'
     with pytest.raises(InputError):
         user_profile_setname(token, long_name, 'last_name')
     with pytest.raises(InputError):
         user_profile_setname(token, '', 'last_name')
 
-def test_invalid_lastname():
+def test_setname_invalid_lastname():
     '''
         invalid test of 0 characters and 26 characters of last name
     '''
     clear()
     auth.auth_register('test1@test.com', 'password', 'user1_first_name', 'user1_last_name')
     token = auth.auth_login('test1@test.com', 'password')['token']
-    long_name = '012345678910'
-    long_name = long_name * 5
+    long_name = '0123456789' * 5 + '1'
     with pytest.raises(InputError):
         user_profile_setname(token, 'first_name', long_name)
     with pytest.raises(InputError):
         user_profile_setname(token, 'first_name', '')
 
+def test_setname_invalid_token():
+    '''
+    test the token does not refer to any valid user
+    '''
+    clear()
+    auth.auth_register('test1@test.com', 'password', 'user1_first_name', 'user1_last_name')
+    with pytest.raises(AccessError):
+        user_profile_setname('invalid_token', 'new_first_name', 'new_last_name')
 
 ###TEST OF SETEMAIL###
 
 def test_profile_setemail_valid():
     '''
-        #valid test for profile setemail
-        #register and login user
-        #update the email
+        valid test for profile setemail
+        register and login user
+        update the email
     '''
     clear()
     u_id = auth.auth_register('test1@test.com', 'password',
@@ -134,11 +150,11 @@ def test_profile_setemail_valid():
     user_profile_setemail(token, 'testvalid@test.com')
     assert user_profile(token, u_id)['user']['email'] == 'testvalid@test.com'
 
-def test_profile_setemail_incorrect_format():
+def test_setemail_invalid_email():
     '''
-        #invalid test for profile setemail
-        #register and login user
-        #give an invalid email
+        invalid test for profile setemail
+        register and login user
+        give an invalid email
     '''
     clear()
     auth.auth_register('test1@test.com', 'password', 'user1_first_name', 'user1_last_name')
@@ -147,11 +163,11 @@ def test_profile_setemail_incorrect_format():
     with pytest.raises(InputError):
         user_profile_setemail(token, 'testvalidtest.com')
 
-def test_profile_setemail_being_used():
+def test_setemail_occupied_email():
     '''
-        #invalid test for the profile email being used
-        #register and login user
-        #give an used email
+        invalid test for the profile email being used
+        register and login user
+        give an used email
     '''
     clear()
     auth.auth_register('test1@test.com', 'password', 'user1_first_name', 'user1_last_name')
@@ -161,10 +177,18 @@ def test_profile_setemail_being_used():
     with pytest.raises(InputError):
         user_profile_setemail(token1, 'test2@test.com')
 
+def test_setemail_invalid_token():
+    '''
+    test the token does not refer to a valid user
+    '''
+    clear()
+    auth.auth_register('test@test.com', 'password', 'name_first', 'name_last')
+    with pytest.raises(AccessError):
+        user_profile_setemail('invalid_token', 'newemail@test.com')
 
 ###TEST OF SETHANDLE###
 
-def test_handle_valid():
+def test_sethandle_valid():
     '''
         valid test for sethandle
     '''
@@ -187,10 +211,10 @@ def test_handle_valid():
     u3_id = auth.auth_register('test3@test.com', 'password',
                                'user3_first_name', 'user3_last_name')['u_id']
     token3 = auth.auth_login('test3@test.com', 'password')['token']
-    user_profile_sethandle(token3, '12345678911234567891')
-    assert user_profile(token3, u3_id)['user']['handle_str'] == '12345678911234567891'
+    user_profile_sethandle(token3, '01234567890123456789')
+    assert user_profile(token3, u3_id)['user']['handle_str'] == '01234567890123456789'
 
-def test_handle_incorrect_length():
+def test_sethandle_invalid_length():
     '''
         invalid test for the incorrect length:
             1. characters less than 3
@@ -203,13 +227,13 @@ def test_handle_incorrect_length():
     with pytest.raises(InputError):
         user_profile_sethandle(token, 'u')
     with pytest.raises(InputError):
-        user_profile_sethandle(token, 'abcdefghijklmnopkistuvwxyz')
+        user_profile_sethandle(token, '012345678901234567890')
     with pytest.raises(InputError):
         user_profile_sethandle(token, '')
     with pytest.raises(InputError):
         user_profile_sethandle(token, '12')
 
-def test_handle_being_used():
+def test_sethandle_being_used():
     '''
         invalid test for the handle has being used
     '''
@@ -227,14 +251,11 @@ def test_handle_being_used():
     with pytest.raises(InputError):
         user_profile_sethandle(token2, temp1_handle_str)
 
-
-### ACCESSERROR ###
-def test_invalid_token():
+def test_sethandle_invalid_token():
     '''
-        invalid token to check
+    test the token does not refer to a valid user
     '''
     clear()
-    u1_id = auth.auth_register('test1@test.com', 'password', 'user1_name', 'user1_name')['u_id']
-    auth.auth_login('test1@test.com', 'password')
+    auth.auth_register('test@test.com', 'password', 'name_first', 'name_last')
     with pytest.raises(AccessError):
-        user_profile('invalidtoken', u1_id)
+        user_profile_sethandle('invalidtoken', 'newhandle')
