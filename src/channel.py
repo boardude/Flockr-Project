82d59bff23f0ff9793c0fef72bfd073f5801b8c6
Daoting <daoting.chen@student.unsw.edu.au>
2020-10-24 02:42:57 +0800

    remove some helper functions to helper.py and message function bug fixed

--- src/channel.py
+++ src/channel.py
@@ -1,9 +1,11 @@
 '''
 import data.py for data storing
 import error.py for error raising
+from helper import some helper functions
 '''
 from error import InputError, AccessError
 from data import users, channels
+from helper import get_user_from_id, get_user_from_token, get_channel_from_id
 
 def channel_invite(token, channel_id, u_id):
     '''
@@ -26,29 +28,33 @@ def channel_invite(token, channel_id, u_id):
             1. the authorised user is not already a member of the channel.
             2. given token does not refer to a valid token
     '''
-    invited_user = uid_to_user(u_id)
-    channel = channelid_to_channel(channel_id)
+    auth_user = get_user_from_token(token)
+    invited_user = get_user_from_id(u_id)
+    channel = get_channel_from_id(channel_id)
+
+    # access error when given token does not refer to a valid user
+    if auth_user is None:
+        raise AccessError()
     # input error when u_id does not refer to a valid user
-    if invited_user is False:
+    if invited_user is None:
         raise InputError()
-
     # input error when channel_id does not refer to a valid channel.
-    if channel is False:
+    if channel is None:
         raise InputError()
 
-    # accesss error when the authorised user is not already a member of the channel
-    if is_user_in_channel(token, channel_id) is False:
+    # accesss error when the authorised user is not a member of the channel
+    if auth_user['u_id'] not in channel['all_members']:
         raise AccessError()
 
-    # already in channel
-    if is_user_in_channel(invited_user['token'], channel_id):
+    # invited_user is already in channel
+    if invited_user['u_id'] in channel['all_members']:
         return {
         }
 
-    channel['all_members'].append(invited_user['u_id'])
+    channel['all_members'].append(u_id)
     invited_user['channels'].append(channel_id)
-    # if u_id refers to the owner of flockr
-    if invited_user['u_id'] == users[0]['u_id']:
+    # if u_id refers to the owner of flockr, channel would set it as an owner
+    if invited_user['permission_id'] == 1:
         channel['owner_members'].append(invited_user['u_id'])
     return {
     }
@@ -78,26 +84,23 @@ def channel_details(token, channel_id):
             1. the authorised user is not already a member of the channel.
             2. given token does not refer to a valid token
     '''
-    channel = channelid_to_channel(channel_id)
-    # inputerror when Channel ID is not a valid channel
-    if channel is False:
-        raise InputError()
+    auth_user = get_user_from_token(token)
+    channel = get_channel_from_id(channel_id)
 
-    # access error when Authorised user is not
-    # a member of channel with channel_id
-    if is_user_in_channel(token, channel_id) is False:
+    # access error when given token does not refer to a valid user
+    if auth_user is None:
+        raise AccessError()
+    # inputerror when Channel ID is not a valid channel
+    if channel is None:
+        raise InputError()
+    # access error when Authorised user is not a member of channel with channel_id
+    if auth_user['u_id'] not in channel['all_members']:
         raise AccessError()
 
-    owner_details = []
-    all_details = []
-    for owner in channel['owner_members']:
-        owner_details.append(uid_to_member(owner))
-    for member in channel['all_members']:
-        all_details.append(uid_to_member(member))
     return {
         'name': channel['name'],
-        'owner_members': owner_details,
-        'all_members': all_details,
+        'owner_members': list(map(member_initial, channel['owner_members'])),
+        'all_members': list(map(member_initial, channel['all_members'])),
     }
 
 def channel_messages(token, channel_id, start):
@@ -116,7 +119,7 @@ def channel_messages(token, channel_id, start):
         param3: start index of messages
 
     Returns:
-        This will return a dictionary with channel details.
+        This will return a dictionary.
         {
             'messages' : (a list of messages),
             'start' : (start index),
@@ -131,33 +134,34 @@ def channel_messages(token, channel_id, start):
             1. Authorised user is not a member of channel with channel_id.
             2. given token does not refer to a valid token
     '''
-    # raise accesserror if given token does not refer to a valid user
-    if token_to_user(token) is False:
+    auth_user = get_user_from_token(token)
+    channel = get_channel_from_id(channel_id)
+    # access error when given token does not refer to a valid user
+    if auth_user is None:
         raise AccessError()
-
-    channel = channelid_to_channel(channel_id)
     # input error when Channel ID is not a valid channel
-    if channel is False:
+    if channel is None:
         raise InputError()
 
+    all_msgs = list(reversed(channel['messages']))
     # input error when start is greater than the total number
     # of messages in the channel
-    if start >= len(channel['messages']):
+    if start >= len(all_msgs):
         raise InputError()
 
     # access error when Authorised user is not a member of channel with channel_id
-    if is_user_in_channel(token, channel_id) is False:
+    if auth_user['u_id'] not in channel['all_members']:
         raise AccessError()
 
     return_messages = []
     end = start + 50
-    if end >= len(channel['messages']):
+    if end >= len(all_msgs):
         end = -1
-        for message in channel['messages'][start:]:
+        for message in all_msgs[start:]:
             return_messages.append(message)
     else:
-        for message in channel['messages'][start:start + 49]:
-            return_messages.append(message)
+        for msg in all_msgs[start:end]:
+            return_messages.append(msg)
     return {
         'messages' : return_messages,
         'start' : start,
@@ -183,26 +187,22 @@ def channel_leave(token, channel_id):
             1. Authorised user is not a member of channel with channel_id
             2. given token does not refer to a valid token
     '''
-    user = token_to_user(token)
+    auth_user = get_user_from_token(token)
+    channel = get_channel_from_id(channel_id)
     # access error when the token does not refer to a valid token
-    if user is False:
+    if auth_user is None:
         raise AccessError()
-
-    channel = channelid_to_channel(channel_id)
     # input error when Channel ID is not a valid channel
-    if channel is False:
+    if channel is None:
         raise InputError()
-
-    # access error when Authorised user is not
-    # a member of channel with channel_id
-    if is_user_in_channel(token, channel_id) is False:
+    # access error when Authorised user is not a member of channel with channel_id
+    if auth_user['u_id'] not in channel['all_members']:
         raise AccessError()
 
-    user['channels'].remove(channel_id)
-    channel['all_members'].remove(user['u_id'])
-    for owner in channel['owner_members']:
-        if owner == user['u_id']:
-            channel['owner_members'].remove(user['u_id'])
+    auth_user['channels'].remove(channel_id)
+    channel['all_members'].remove(auth_user['u_id'])
+    if auth_user['u_id'] in channel['owner_members']:
+        channel['owner_members'].remove(auth_user['u_id'])
     return {
     }
 
@@ -226,31 +226,29 @@ def channel_join(token, channel_id):
                 (when the authorised user is not a global owner)
             2. given token does not refer to a valid token
     '''
-    user = token_to_user(token)
+    auth_user = get_user_from_token(token)
+    channel = get_channel_from_id(channel_id)
     # access error when the token does not refer to a valid token
-    if user is False:
+    if auth_user is None:
         raise AccessError()
-
-    channel = channelid_to_channel(channel_id)
     # input error when Channel ID is not a valid channel
-    if channel is False:
+    if channel is None:
         raise InputError()
-
     # access error when channel_id refers to a channel that is private
     # the authorised user is not a global owner
-    if channel['public'] is False and user is not users[0]:
+    if channel['public'] is False and auth_user['permission_id'] != 1:
         raise AccessError()
 
     # already in the channel
-    if is_user_in_channel(token, channel_id) is True:
+    if channel_id in auth_user['channels']:
         return {
         }
 
-    channel['all_members'].append(user['u_id'])
-    user['channels'].append(channel_id)
+    channel['all_members'].append(auth_user['u_id'])
+    auth_user['channels'].append(channel_id)
     # owner of flockr will become the owner of channel
-    if user['u_id'] == users[0]['u_id']:
-        channel['owner_members'].append(user['u_id'])
+    if auth_user['permission_id'] == 1:
+        channel['owner_members'].append(auth_user['u_id'])
     return {
     }
 
@@ -277,30 +275,29 @@ def channel_addowner(token, channel_id, u_id):
                 the flockr, or an owner of this channel
             2. given token does not refer to a valid token
     '''
-    inviter = token_to_user(token)
+    auth_user = get_user_from_token(token)
+    channel = get_channel_from_id(channel_id)
+    invited_user = get_user_from_id(u_id)
     # access error when the token does not refer to a valid token
-    if inviter is False:
+    if auth_user is None:
         raise AccessError()
 
-    channel = channelid_to_channel(channel_id)
     # input error when Channel ID is not a valid channel
-    if channel is False:
+    if channel is None:
         raise InputError()
 
+    # input error when u_id does not refer to a valid user
+    if invited_user is None:
+        raise InputError()
+    
     # input error when When user with user id u_id
     # is already an owner of the channel
-    for owner in channel['owner_members']:
-        if owner == u_id:
-            raise InputError()
+    if invited_user['u_id'] in channel['owner_members']:
+        raise InputError()
 
     # access error when the authorised user is not
     # an owner of the flockr, or an owner of this channel
-    permitted = False
-    for owner in channel['owner_members']:
-        if owner == inviter['u_id']:
-            permitted = True
-            break
-    if permitted is not True:
+    if auth_user['permission_id'] != 1 and auth_user['u_id'] not in channel['owner_members']:
         raise AccessError()
 
     channel['owner_members'].append(u_id)
@@ -331,86 +328,36 @@ def channel_removeowner(token, channel_id, u_id):
                 the flockr, or an owner of this channel
             2. given token does not refer to a valid token
     '''
-    remover = token_to_user(token)
-    # access error when the token does not refer to a valid token
-    if remover is False:
+    auth_user = get_user_from_token(token)
+    channel = get_channel_from_id(channel_id)
+    removed_user = get_user_from_id(u_id)
+    # access error when given token does not refer to a valid user
+    if auth_user is None:
         raise AccessError()
-
-    channel = channelid_to_channel(channel_id)
     # input error when Channel ID is not a valid channel
-    if channel is False:
+    if channel is None:
         raise InputError()
-
-    # input error when user with user id u_id is not
-    # an owner of the channel
-    is_owner = False
-    for owner in channel['owner_members']:
-        if owner == u_id:
-            is_owner = True
-            break
-    if is_owner is False:
+    # input error when u_id does not refer to a valid user
+    if removed_user is None:
+        raise InputError()
+    # input error when user with user id u_id is not an owner of the channel
+    if removed_user['u_id'] not in channel['owner_members']:
         raise InputError()
 
     # accesss error when the authorised user is not
     # an owner of the flockr, or an owner of this channel
-    permitted = False
-    for owner in channel['owner_members']:
-        if owner == remover['u_id']:
-            permitted = True
-            break
-    if permitted is not True:
+    if auth_user['permission_id'] != 1 and auth_user['u_id'] not in channel['owner_members']:
         raise AccessError()
 
-    if u_id != users[0]['u_id']:
+    # program will ignore the request if u_id refers to the owner of flockr
+    if removed_user['permission_id'] != 1:
         channel['owner_members'].remove(u_id)
     return {
     }
 
 ########## help functions ##########
 
-def token_to_user(token):
-    '''
-    This is a simple helper function.
-    It will return a user with given token if it exists in data,
-    else return False
-
-    Args:
-        param1: token
-
-    Returns:
-        This will return uesr(dictionary) if token refers to a valid user in data,
-        else return False.
-
-    Raises:
-        this will not raise any error
-    '''
-    for user in users:
-        if user['token'] == token:
-            return user
-    return False
-
-def uid_to_user(u_id):
-    '''
-    This is a simple helper function.
-    It will return a user with given u_id if it exists in data,
-    else return False
-
-    Args:
-        param1: u_id
-
-    Returns:
-        This will return uesr(dictionary) if u_id refers to a valid user in data,
-        else return False.
-
-    Raises:
-        this will not raise any error
-    '''
-    for user in users:
-        if user['u_id'] == u_id:
-            return user
-    return False
-
-def uid_to_member(u_id):
+def member_initial(u_id):
     '''
     This is a simple helper function.
     It will return a member type data with given u_id if it exists in data,
@@ -426,54 +373,9 @@ def uid_to_member(u_id):
     Raises:
         this will not raise any error
     '''
-    user = uid_to_user(u_id)
+    user = get_user_from_id(u_id)
     return {
         'u_id' : user['u_id'],
         'name_first' : user['name_first'],
-        'name_last' : user['name_last']
+        'name_last' : user['name_last'],
     }
-
-def channelid_to_channel(channel_id):
-    '''
-    This is a simple helper function.
-    It will return a channel with given channel_id if it exists in data,
-    else return False
-
-    Args:
-        param1: channel_id
-
-    Returns:
-        This will return channel(dictionary) if token refers to a valid chanenl in data,
-        else return False.
-
-    Raises:
-        this will not raise any error
-    '''
-    for channel in channels:
-        if channel['channel_id'] == channel_id:
-            return channel
-    return False
-
-def is_user_in_channel(token, channel_id):
-    '''
-    This is a simple helper function.
-    It will test whether a user with given token is in target channel.
-
-    Args:
-        param1: token
-
-    Returns:
-        This will return a boolean value.
-        It will return True if the user is in target channel.
-        else return False.
-
-    Raises:
-        this will not raise any error
-    '''
-    user = token_to_user(token)
-    if user is False:
-        return False
-    for channel in user['channels']:
-        if channel == channel_id:
-            return True
-    return False
