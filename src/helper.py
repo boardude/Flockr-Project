0e8a7c060c1a896bb198996454dbd6c6b72e874f
Daoting <daoting.chen@student.unsw.edu.au>
2020-10-24 02:48:07 +0800

    remove testing helper function and update codes for encoded token

--- src/helper.py
+++ src/helper.py
@@ -1,74 +1,81 @@
-import random
-import string
+import jwt
 from data import users, channels
-from auth import auth_register, auth_login
 
-############ UNIVERSAL HELPER FUNCTIONS #########
-def register_and_login(email, password, first_name, last_name):
-    """
-        Registers and logs in user with provided details,
-        returning the token
+SECRET = 'grape6'
 
-    """
-    auth_register(email, password, first_name, last_name)
-    login = auth_login(email, password)
-    return login['token']
-
-def is_token_valid(token):
-    """
-        Returns True if token is valid (token is found in users list), otherwise False
-    """
-    for user in users:
-        if user['token'] is token:
-            return True
-    return False
-
-def get_uid_from_token(token):
-    """
-        Return the corresponding u_id when given the token of an authorised
-        user
-    """
-    for user in users:
-        if user['token'] == token:
-            return user['u_id']
-
-    return None
-
-def get_random_str(length):
-    """
-        Generates random string with the combination of lower-
-        and upper-case letters
-    """
-    letters = string.ascii_letters
-    random_str = ''.join(random.choice(letters) for i in range(length))
-
-    return random_str
-
-def get_user_from_token_naive(token): # temporary function
+def get_user_from_email(email):
     '''
-    no decode
+    This is a simple helper function to test email duplication.
+    This will return corresponding user data if email is duplicate, else return None
+
+    Args:
+        param1: email
+
+    Returns:
+        This will return user(a dictionary) if the email belongs to an
+        exsiting user, else return None.
     '''
     for user in users:
-        if user['token'] == token:
+        if user['email'] == email:
             return user
+    return None
 
 def get_user_from_id(u_id):
     '''
-        Returns a user with given u_id if it exists in data,
-        else return None
+    This is a simple helper function.
+    It will return a user with given u_id if it exists in data,
+    else return False
+
+    Args:
+        param1: u_id
+
+    Returns:
+        This will return uesr(dictionary) if u_id refers to a valid user in data,
+        else return False.
+    '''
+    for user in users:
+        if user['u_id'] == u_id:
+            return user
+    return None
+
+def get_user_from_token(token):
+    '''
+    This is a simple helper function.
+    It will return a user with given token can be decoded and is a login user,
+    else return none
+
+    Args:
+        param1: u_id
+
+    Returns:
+        This will return uesr(dictionary) if token can be decoded and user has login,
+        else return False.
+
+    Raises:
+        this will not raise any error
     '''
     try:
-        return users[u_id-1]
-    except IndexError:
+        info = jwt.decode(token.encode('utf-8'), SECRET, algorithms=['HS256'])
+        if info['has_login'] is False:
+            return None
+        return get_user_from_id(info['u_id'])
+    except:
         return None
 
 def get_channel_from_id(channel_id):
     '''
-        Return a channel with given channel_id if it exists in data,
-        else return None
-    '''
-    try:
-        return channels[channel_id-1]
-    except IndexError:
-        return None
+    This is a simple helper function.
+    It will return a channel with given channel_id if it exists in data,
+    else return None
 
+    Args:
+        param1: channel_id
+
+    Returns:
+        This will return channel(dictionary) if token refers to a valid chanenl in data,
+        else return False.
+    '''
+    for channel in channels:
+        if channel['channel_id'] == channel_id:
+            return channel
+    return None
