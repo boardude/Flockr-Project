55cf22ac68d61fb4c2942579c6dbfab1c1451bc2
Daoting <daoting.chen@student.unsw.edu.au>
2020-10-24 16:23:47 +0800

    finish wrapping auth and clear function

--- src/server.py
+++ src/server.py
@@ -1,8 +1,10 @@
 import sys
+from auth import auth_login, auth_logout, auth_register
 from json import dumps
 from flask import Flask, request
 from flask_cors import CORS
 from error import InputError
+from other import clear
 
 def defaultHandler(err):
     response = err.get_response()
@@ -30,6 +32,32 @@ def echo():
     return dumps({
         'data': data
     })
+
+########################################
+############## auth.py #################
+########################################
+@APP.route('/auth/login', methods=['POST'])
+def login():
+    input = request.get_json()
+    return dumps(auth_login(input['email'], input['password']))
+
+@APP.route('/auth/logout', methods=['POST'])
+def logout():
+    input = request.get_json()
+    return dumps(auth_logout(input['token']))
+
+@APP.route('/auth/register', methods=['POST'])
+def register():
+    input = request.get_json()
+    return dumps(auth_register(input['email'], input['password'],
+                    input['name_first'], input['name_last']))
+
+########################################
+############## other.py ################
+########################################
+@APP.route('/clear', methods=['DELETE'])
+def clear_data():
+    return dumps(clear())
 
 if __name__ == "__main__":
     APP.run(port=0) # Do not edit this port
