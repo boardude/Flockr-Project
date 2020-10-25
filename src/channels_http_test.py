2c4e771bc99e65fe4a99220ef05ea0e6c59c5ed1
Daoting <daoting.chen@student.unsw.edu.au>
2020-10-25 23:39:02 +0800

    bug fixed with all tests

--- src/channels_http_test.py
+++ src/channels_http_test.py
@@ -36,14 +36,17 @@ def create_users(url):
 
     # register & log in 2 users
     for i in range(2):
+        password = 'validpass' + str(i+1)
+        email = 'validuser' + str(i+1) + 'email@gmail.com'
         user_data = {
-            'password' : 'validpass' + str(i+1),
+            'password' : password,
             'name_first': 'User',
             'name_last' : '0' + str(i+1),
-            'email': 'validuser' + str(i+1) + 'email@gmail.com',
+            'email': email,
         }
         requests.post(url + 'auth/register', json=user_data)
-        resp = requests.post(url + 'auth/login', json=user_data)
+        requests.post(url + 'auth/login', json={'email': email, 'password': password})
+
 
 @pytest.fixture
 def create_channels(url):
@@ -53,145 +56,31 @@ def create_channels(url):
     """
     channel_data = {
         'token': token_generate(1, 'login'),
-        'channel_name': 'Channel 01',
+        'name': 'Channel 01',
         'is_public': True,
     }
     requests.post(url + 'channels/create', json=channel_data)
 
     channel_data = {
         'token': token_generate(1, 'login'),
-        'channel_name': 'Channel 02',
-        'is_public': False,
-    }
-    requests.post(url + 'channels/create', json=channel_data)
-
-    channel_data = {
-        'token': token_generate(1, 'login'),
-        'channel_name': 'Channel 03',
-        'is_public': True,
-    }
-    requests.post(url + 'channels/create', json=channel_data)
-
-    channel_data = {
-        'token': token_generate(1, 'login'),
-        'channel_name': 'Channel 03',
-        'is_public': True,
-    }
-    requests.post(url + 'channels/create', json=channel_data)
-
-    channel_data = {
-        'token': token_generate(2, 'login'),
-        'channel_name': 'Channel 04 User 02',
-        'is_public': True,
-    }
-    requests.post(url + 'channels/create', json=channel_data)
-
-    channel_data = {
-        'token': token_generate(2, 'login'),
-        'channel_name': 'Channel 05 User 02',
+        'name': 'Channel 02',
         'is_public': False,
     }
     requests.post(url + 'channels/create', json=channel_data)
 
     channel_data = {
         'token': token_generate(2, 'login'),
-        'channel_name': 'Channel 06 User 02',
-        'is_public': False,
+        'name': 'Channel 03 User 02',
+        'is_public': True,
     }
     requests.post(url + 'channels/create', json=channel_data)
 
-def test_http_list_invalid_token(url, create_users, create_channels):
-    """
-        Test for AccessError exception thrown by channels_create() when token
-        passed in is not a valid token
-    """
-    # empty
-    r = requests.get(url + 'channels/list', params={'token': ''})
-    assert r.status_code == 400
-
-    # None
-    r = requests.get(url + 'channels/list', params={'token': None})
-    assert r.status_code == 400
-
-    # Not the correct data type
-    r = requests.get(url + 'channels/list', params={'token': 123})
-    assert r.status_code == 400
-
-    # Not an authorised user
-    bad_token = 'invalid_token'
-    r = requests.get(url + 'channels/list', params={'token': bad_token})
-    assert r.status_code == 400
-
-def test_http_list_standard(url, create_users, create_channels):
-    """
-        Test for standard functionality of channels_list() according to spec
-    """
-    r = requests.get(url + 'channels/list', params={'token': token_generate(1, 'login')})
-    payload = r.json()
-
-    # test length and accuracy of returned channels list, making sure 
-    # channels of User 2 is not listed
-    assert len(payload['channels']) == 3
-    channel_01_listed = payload['channels'][0]
-    channel_02_listed = payload['channels'][1]
-    channel_03_listed = payload['channels'][2]
-    assert channel_01_listed['channel_id'] == channel_01['channel_id']
-    assert channel_01_listed['name'] == 'Channel 01'
-    assert channel_02_listed['channel_id'] == channel_02['channel_id']
-    assert channel_02_listed['name'] == 'Channel 02'
-    assert channel_03_listed['channel_id'] == channel_03['channel_id']
-    assert channel_03_listed['name'] == 'Channel 03'
-
-def test_http_listall_invalid_token(url, create_users, create_channels):
-    """
-        Test for AccessError exception thrown by channels_create() when token
-        passed in is not a valid token
-    """
-    # empty
-    r = requests.get(url + 'channels/listall', params={'token': ''})
-    assert r.status_code == 400
-
-    # None
-    r = requests.get(url + 'channels/listall', params={'token': None})
-    assert r.status_code == 400
-
-    # Not the correct data type
-    r = requests.get(url + 'channels/listall', params={'token': 123})
-    assert r.status_code == 400
-
-    # Not an authorised user
-    bad_token = 'invalid_token'
-    r = requests.get(url + 'channels/listall', params={'token': bad_token})
-    assert r.status_code == 400
-
-def test_http_listall_standard(url, create_users, create_channels):
-    """
-        Test for standard functionality of channels_list() according to spec
-    """
-    r = requests.get(url + 'channels/listall', params={'token': token_generate(1, 'login')})
-    payload = r.json()
-
-    # test length and accuracy of returned channels list, making sure 
-    # channels of User 2 is not listed
-    assert len(payload['channels']) == 6
-    channel_01_listed = payload['channels'][0]
-    channel_02_listed = payload['channels'][1]
-    channel_03_listed = payload['channels'][2]
-    channel_04_listed = payload['channels'][3]
-    channel_05_listed = payload['channels'][4]
-    channel_06_listed = payload['channels'][5]
-    assert channel_01_listed['channel_id'] == channel_01['channel_id']
-    assert channel_01_listed['name'] == 'Channel 01'
-    assert channel_02_listed['channel_id'] == channel_02['channel_id']
-    assert channel_02_listed['name'] == 'Channel 02'
-    assert channel_03_listed['channel_id'] == channel_03['channel_id']
-    assert channel_03_listed['name'] == 'Channel 03'
-    assert channel_04_listed['channel_id'] == channel_04['channel_id']
-    assert channel_04_listed['name'] == 'Channel 04 User 2'
-    assert channel_05_listed['channel_id'] == channel_05['channel_id']
-    assert channel_05_listed['name'] == 'Channel 05 User 2'
-    assert channel_06_listed['channel_id'] == channel_06['channel_id']
-    assert channel_06_listed['name'] == 'Channel 06 User 2'
+    channel_data = {
+        'token': token_generate(2, 'login'),
+        'name': 'Channel 04 User 02',
+        'is_public': False,
+    }
+    requests.post(url + 'channels/create', json=channel_data)
 
 def test_http_create_invalid_name(url, create_users):
     """
@@ -200,11 +89,11 @@ def test_http_create_invalid_name(url, create_users):
     """
     query = {
         'token': token_generate(1, 'login'),
-        'channel_name': 'Channel NameThatHasMoreThanTwentyCharacters',
+        'name': 'n' * 21,
         'is_public': True,
     }
-    r = requests.post(url + 'channels/create', json=query)
-    assert r.status_code == 400
+    resp = requests.post(url + 'channels/create', json=query)
+    assert resp.status_code == 400
 
 def test_http_create_standard(url, create_users):
     """
@@ -212,24 +101,129 @@ def test_http_create_standard(url, create_users):
     """
     query = {
         'token': token_generate(1, 'login'),
-        'channel_name': 'Channel 01',
+        'name': 'Channel 01',
         'is_public': True,
     }
-    r = requests.post(url + 'channels/create', json=query)
-    assert r.status_code == 200
+    resp = requests.post(url + 'channels/create', json=query)
+    assert resp.status_code == 200
+    query['is_public'] = False
+    resp = requests.post(url + 'channels/create', json=query)
+    assert resp.status_code == 200
 
-def test_http_create_duplicate(url, create_users):
+def test_http_create_invalid_token(url, create_users):
     """
         When two channels with duplicate details are created
         Ensure both are created as they differ by channel_id
     """    
     query = {
-        'token': token_generate(1, 'login'),
-        'channel_name': 'Channel 01',
+        'token': token_generate(1, 'logout'),
+        'name': 'Channel 01',
         'is_public': True,
     }
-    r = requests.post(url + 'channels/create', json=query)
-    assert r.status_code == 200
+    resp = requests.post(url + 'channels/create', json=query)
+    assert resp.status_code == 400
+    query['token'] = 'invalid_token'
+    resp = requests.post(url + 'channels/create', json=query)
+    assert resp.status_code == 400
 
-    r = requests.post(url + 'channels/create', json=query)
-    assert r.status_code == 200
+def test_http_list_invalid_token(url, create_users, create_channels):
+    """
+        Test for AccessError exception thrown by channels_create() when token
+        passed in is not a valid token
+    """
+    # empty
+    resp = requests.get(url + 'channels/list', params={'token': ''})
+    assert resp.status_code == 400
+
+    # None
+    resp = requests.get(url + 'channels/list', params={'token': None})
+    assert resp.status_code == 400
+
+    # Not the correct data type
+    resp = requests.get(url + 'channels/list', params={'token': 123})
+    assert resp.status_code == 400
+
+    # Not an authorised user
+    bad_token = 'invalid_token'
+    resp = requests.get(url + 'channels/list', params={'token': bad_token})
+    assert resp.status_code == 400
+
+def test_http_list_standard(url, create_users, create_channels):
+    """
+        Test for standard functionality of channels_list() according to spec
+    """
+    resp = requests.get(url + 'channels/list', params={'token': token_generate(1, 'login')})
+    payload = resp.json()
+    assert resp.status_code == 200
+
+    # test length and accuracy of returned channels list, making sure
+    # channels of User 2 is not listed
+    assert len(payload['channels']) == 2
+    channel_01_listed = payload['channels'][0]
+    channel_02_listed = payload['channels'][1]
+    assert channel_01_listed['channel_id'] == 1
+    assert channel_01_listed['name'] == 'Channel 01'
+    assert channel_02_listed['channel_id'] == 2
+    assert channel_02_listed['name'] == 'Channel 02'
+
+def test_http_list_standard_no_channel(url, create_users):
+    """
+        Test for standard functionality of channels_list() according to spec
+    """
+    resp = requests.get(url + 'channels/list', params={'token': token_generate(1, 'login')})
+    payload = resp.json()
+    assert resp.status_code == 200
+    assert len(payload['channels']) == 0
+
+def test_http_listall_invalid_token(url, create_users, create_channels):
+    """
+        Test for AccessError exception thrown by channels_create() when token
+        passed in is not a valid token
+    """
+    # empty
+    resp = requests.get(url + 'channels/listall', params={'token': ''})
+    assert resp.status_code == 400
+
+    # None
+    resp = requests.get(url + 'channels/listall', params={'token': None})
+    assert resp.status_code == 400
+
+    # Not the correct data type
+    resp = requests.get(url + 'channels/listall', params={'token': 123})
+    assert resp.status_code == 400
+
+    # Not an authorised user
+    resp = requests.get(url + 'channels/listall', params={'token': 'invalid_token'})
+    assert resp.status_code == 400
+
+def test_http_listall_standard(url, create_users, create_channels):
+    """
+        Test for standard functionality of channels_list() according to spec
+    """
+    resp = requests.get(url + 'channels/listall', params={'token': token_generate(1, 'login')})
+    payload = resp.json()
+
+    # test length and accuracy of returned channels list, making sure 
+    # channels of User 2 is not listed
+    assert len(payload['channels']) == 4
+    channel_01_listed = payload['channels'][0]
+    channel_02_listed = payload['channels'][1]
+    channel_03_listed = payload['channels'][2]
+    channel_04_listed = payload['channels'][3]
+    assert channel_01_listed['channel_id'] == 1
+    assert channel_01_listed['name'] == 'Channel 01'
+    assert channel_02_listed['channel_id'] == 2
+    assert channel_02_listed['name'] == 'Channel 02'
+    assert channel_03_listed['channel_id'] == 3
+    assert channel_03_listed['name'] == 'Channel 03 User 02'
+    assert channel_04_listed['channel_id'] == 4
+    assert channel_04_listed['name'] == 'Channel 04 User 02'
+
+def test_http_listall_standard_no_channel(url, create_users):
+    """
+        Test for standard functionality of channels_listall() according to spec
+    """
+    resp = requests.get(url + 'channels/list', params={'token': token_generate(1, 'login')})
+    payload = resp.json()
+    assert resp.status_code == 200
+    assert len(payload['channels']) == 0
